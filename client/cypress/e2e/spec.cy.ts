c6598b49e45922c0ffb336e01e7c89faee2e5e76
Wojciech <szmelczerczykwojciech@gmail.com>
2022-09-10 16:47:26 +0200

    e2e create note tests implemented

--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -1,9 +1,36 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:5000/");
-    cy.get("input.xxx").type("user404@gmail.com");
+describe("Login Component", () => {
+  it("if user credentials are incorrect, prompt error", () => {
+    cy.visit("http://localhost:5000/login");
+    cy.get("input.xxx").type("user2404gmail.com");
     cy.get("input.yyy").type("test404");
     cy.get("button.userBtn").click();
     cy.get("div.zzz").should("contain", "Please enter a valid email");
+  });
+
+  it("if user credentials are correct, redirect to note list page", () => {
+    cy.visit("http://localhost:5000/login");
+    cy.get("input.xxx").type("user2@gmail.com");
+    cy.get("input.yyy").type("test123");
+    cy.get("button.userBtn").click();
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq("http://localhost:5000/");
+    });
+  });
+});
+
+describe("CreateNote Component", () => {
+  it("if note title is too short, prompt an error", () => {
+    localStorage.setItem("at", "");
+    cy.visit("http://localhost:5000/createNote");
+    cy.get("input.noteTitleInput").type("tes");
+    cy.get("button.createNoteButton").click();
+    cy.get("div.createNoteError").should("contain", "Note title is too short.");
+  });
+  it("if note title is too long, prompt an error", () => {
+    localStorage.setItem("at", "");
+    cy.visit("http://localhost:5000/createNote");
+    cy.get("input.noteTitleInput").type("testtest1");
+    cy.get("button.createNoteButton").click();
+    cy.get("div.createNoteError").should("contain", "Note title is too long.");
   });
 });
