8fa3be1d8aee8a18632860583198c033e485d268
Wojciech <szmelczerczykwojciech@gmail.com>
2023-01-06 12:43:17 +0100

    minor change

--- client/cypress/e2e/note.cy.ts
+++ client/cypress/e2e/note.cy.ts
@@ -2,10 +2,7 @@ import { tooLongTitle, tooShortTitle } from "../fixtures/note.json";
 
 describe("Create note", () => {
   beforeEach(() => {
-    localStorage.setItem(
-      "at",
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOWY5OWVhNGRhODYzMGIwYWNjYWI5YyIsImlhdCI6MTY3MjY1NTM3OSwiZXhwIjoxNjcyNjU2Mjc5fQ.EV3kYep5HPRNnK_X9a8GJ3N1byVSF625Fzig_ug5whQ"
-    );
+    localStorage.setItem("at", process.env.REACT_APP_JWT);
   });
 
   it("if note title is too short, prompt an error", () => {
