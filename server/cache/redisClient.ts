f8270bebae83104ada4d2756aa81a5d2be1daa3c
Wojciech <szmelczerczykwojciech@gmail.com>
2023-02-05 17:52:27 +0100

    cache removed

--- server/cache/redisClient.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "redis";
-
-const redisClient = createClient();
-
-redisClient.on("error", () => console.log("redis cache db error"));
-
-redisClient.connect().then(() => console.log("connected to redis cache db"));
-
-export default redisClient;
